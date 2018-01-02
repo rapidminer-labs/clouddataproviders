c3c1c1d953839a30a9eecb91892cdb49d0283532
Bhupendra Patil <bpatil@rapidminer.com>
2018-01-02 11:56:21 +0000

    Working operator minus documentation not showing up

--- src/main/java/com/rapidminer/extension/twingly/operator/SearchBlogsOperator.java
+++ src/main/java/com/rapidminer/extension/twingly/operator/SearchBlogsOperator.java
@@ -20,7 +20,12 @@ import com.rapidminer.operator.io.AbstractExampleSource;
 import com.rapidminer.operator.ports.OutputPort;
 import com.rapidminer.operator.preprocessing.filter.Real2Integer;
 import com.rapidminer.parameter.ParameterType;
+import com.rapidminer.parameter.ParameterTypeBoolean;
+import com.rapidminer.parameter.ParameterTypeCategory;
+import com.rapidminer.parameter.ParameterTypeInt;
+import com.rapidminer.parameter.ParameterTypeList;
 import com.rapidminer.parameter.ParameterTypeString;
+import com.rapidminer.parameter.UndefinedParameterError;
 import com.rapidminer.tools.Ontology;
 import com.rapidminer.tools.config.ConfigurationException;
 import com.rapidminer.tools.config.ConfigurationManager;
@@ -33,10 +38,22 @@ import com.twingly.search.domain.Result;
 
 public class SearchBlogsOperator extends AbstractExampleSource {
 
-	private static final String SEARCH_TERM = "Search Term";
+	private static final String Query_TERM = "Query Term";
 	// private InputPort inputPort;
 	// private OutputPort outputPort;
 	private static final String PARAMETER_CONFIG = "API Connection";
+	private static final String SEARCH_TEXT = "Search Text";
+	private static final String SEARCH_TITLE = "Search Title";
+	private static final String SEARCH_URL = "Search URL ";
+	private static final String SEARCH_AUTHOR = "Search Author";
+	private static final String SEARCH_TAGS = "Search Tags";
+	private static final String SEARCH_LINKS = "Search Links";
+	private static final String SEARCH_BLOGNAME = "Search Blogname";
+	private static final String SEARCH_BLOGURL = "Search BlogURL";
+	private static final String TIMESPAN = "Timespan";
+	private static final String MAXPAGE = "Maximum Number of Pages";
+	private static final String SORTBY = "Sort By for search Results";
+	private static final String SORTORDER = "Sort Order for search Results";
 
 	/**
 	 * Default operator constructor.
@@ -59,11 +76,61 @@ public class SearchBlogsOperator extends AbstractExampleSource {
 		type.setOptional(false);
 		parameterTypes.add(type);
 
-		type = new ParameterTypeString(SEARCH_TERM,
+		type = new ParameterTypeString(Query_TERM,
 				"Search Term", "");
 		type.setOptional(false);
 		parameterTypes.add(type);
+		
+		type = new ParameterTypeCategory(TIMESPAN, "Time Span To Search",new String[]{"Anytime","h","12h","24h","w","m","3m"},0,false);
+		parameterTypes.add(type);
+		
+		
+		
+		type = new ParameterTypeInt(MAXPAGE, "Maximum number of pages to bring back, each page is 1000 articles", 1, 100, 5,false);
+		parameterTypes.add(type);
 
+		
+
+		type = new ParameterTypeCategory(SORTBY, "Sort By for Search",new String[]{"published","created","inlinks","twinglyrank"},0,false);
+		parameterTypes.add(type);
+	
+		type = new ParameterTypeCategory(SORTORDER, "Sort Order for Search",new String[]{"asc","desc"},1,false);
+		parameterTypes.add(type);
+		
+		
+		type = new ParameterTypeBoolean(SEARCH_TEXT,"Search Term in Text of the blog", true);
+		type.setExpert(true);
+		parameterTypes.add(type);
+
+		type = new ParameterTypeBoolean(SEARCH_TITLE,"Search Term in title of the blog", true);
+		type.setExpert(true);
+		parameterTypes.add(type);
+		
+		type = new ParameterTypeBoolean(SEARCH_URL,"Search Term in url of the blog", true);
+		type.setExpert(true);
+		parameterTypes.add(type);
+		
+		type = new ParameterTypeBoolean(SEARCH_AUTHOR,"Search Term in author of the blog", true);
+		type.setExpert(true);
+		parameterTypes.add(type);
+		
+		type = new ParameterTypeBoolean(SEARCH_TAGS,"Search Term in tags for the blog", true);
+		type.setExpert(true);
+		parameterTypes.add(type);
+		
+		type = new ParameterTypeBoolean(SEARCH_LINKS,"Search Term in links for  the blog", true);
+		type.setExpert(true);
+		parameterTypes.add(type);
+		
+		type = new ParameterTypeBoolean(SEARCH_BLOGNAME,"Search Term in blogs name ", true);
+		type.setExpert(true);
+		parameterTypes.add(type);
+		
+		type = new ParameterTypeBoolean(SEARCH_BLOGURL,"Search Term in blog url ", true);
+		type.setExpert(true);
+		parameterTypes.add(type);
+		
+		
 		return parameterTypes;
 	}
 
@@ -90,28 +157,8 @@ public class SearchBlogsOperator extends AbstractExampleSource {
 							getProcess().getRepositoryAccessor())
 					.getParameter("API_KEY");
 
-			// create Client object using your API key
-			Client client = new UrlConnectionClient(apiKey);
-
-			String query = getParameterAsString(SEARCH_TERM);
-
-			Result result = client.makeRequest(query);
-
-			// Get the total number of matches returned
-			int numberOfMatchesReturned = result.getNumberOfMatchesReturned();
-			// Get the total number of matches to the search query
-			int numberOfMatchesTotal = result.getNumberOfMatchesTotal();
-			// Get number of seconds it took to execute query
-			double secondsElapsed = result.getSecondsElapsed();
-
-			getLogger().info(
-					"Matches Returned"
-							+ String.valueOf(numberOfMatchesReturned));
-			getLogger().info(
-					"Total number of Matches"
-							+ String.valueOf(numberOfMatchesTotal));
-			getLogger()
-					.info("Seconds Elapsed" + String.valueOf(secondsElapsed));
+			
+			
 
 			Attribute blogid = AttributeFactory.createAttribute("Blog Id",
 					Ontology.NOMINAL);
@@ -169,9 +216,53 @@ public class SearchBlogsOperator extends AbstractExampleSource {
 
 			double[] row = new double[20];
 
+			// create Client object using your API key
+			Client client = new UrlConnectionClient(apiKey);
+			String query =  createSearchString(1);
+			getLogger().info(">>>>>" + query + "<<<<<");
+
+			
+			
+			Result result = client.makeRequest(query);
+
+			// Get the total number of matches returned
+			int numberOfMatchesReturned = result.getNumberOfMatchesReturned();
+			// Get the total number of matches to the search query
+			int numberOfMatchesTotal = result.getNumberOfMatchesTotal();
+			// Get number of seconds it took to execute query
+			double secondsElapsed = result.getSecondsElapsed();
+
+			int pageCount=  getNumberofPages(result);
+			
+			
+			
+			getLogger().info(
+					"Matches Returned"
+							+ String.valueOf(numberOfMatchesReturned));
+			getLogger().info(
+					"Total number of Matches"
+							+ String.valueOf(numberOfMatchesTotal));
+			getLogger()
+					.info("Seconds Elapsed" + String.valueOf(secondsElapsed));
+			
+			
+			for(int pageCounter = 0; pageCounter < pageCount && pageCounter <  getParameterAsInt(MAXPAGE); pageCounter++)
+			{
+			 if(pageCounter == 0)
+			 {
+				 
+			 }
+			 else
+			 {
+					query =  createSearchString(pageCounter+1);
+					getLogger().info(">>>>>" + query + "<<<<<");
+					result = client.makeRequest(query);
+			 }
+			
+			
 			for (Post p : result.getPosts()) {
 
-				getLogger().info("adding row info");
+				//getLogger().info("adding row info");
 
 				row[0] = blogid.getMapping().mapString(
 						replaceNull(p.getBlogId()));
@@ -225,6 +316,8 @@ public class SearchBlogsOperator extends AbstractExampleSource {
 
 				exampleSetBuilder.addDataRow(new DoubleArrayDataRow(row));
 			}
+			
+			}//morethan one page
 			return exampleSetBuilder.build();
 		} catch (ConfigurationException e) {
 			// TODO Auto-generated catch block
@@ -234,4 +327,140 @@ public class SearchBlogsOperator extends AbstractExampleSource {
 		return null;
 	}
 
+	private int getNumberofPages(Result result) {
+		// TODO Auto-generated method stub
+		
+		return (int) Math.ceil(result.getNumberOfMatchesTotal() / 1000);
+		
+		
+	}
+
+	private String createSearchString(Integer PageNumber) throws UndefinedParameterError {
+		// TODO Auto-generated method stub
+		StringBuilder builder = new StringBuilder();
+		
+		//QueryBuilder qb =  QueryBuilder.create(getParameterAsString(Query_TERM));
+		
+		
+		if(!getParameterAsString(TIMESPAN).equals("Anytime"))
+		{
+			builder.append("tspan:"+ getParameter(TIMESPAN) + " ");
+		}
+		
+		builder.append("sort:" + getParameter(SORTBY) + " ");
+		builder.append("sort-order:" + getParameter(SORTORDER) + " ");
+		
+		builder.append("page:" + String.valueOf(PageNumber)+ " ");
+		
+		if(		getParameterAsBoolean(SEARCH_AUTHOR) 
+				&& getParameterAsBoolean(SEARCH_BLOGNAME)
+				&& getParameterAsBoolean(SEARCH_BLOGURL)
+				&& getParameterAsBoolean(SEARCH_LINKS)
+				&& getParameterAsBoolean(SEARCH_TAGS)
+				&& getParameterAsBoolean(SEARCH_TEXT)
+				&& getParameterAsBoolean(SEARCH_TITLE)
+				&& getParameterAsBoolean(SEARCH_URL)
+				
+				)
+				{
+				//	builder.append( getParameterAsString(Query_TERM));
+				}
+		else
+		{
+			builder.append("field:");
+			Integer added = 0;
+			if(getParameterAsBoolean(SEARCH_AUTHOR)) {added=1; builder.append("author");}
+			
+			if(getParameterAsBoolean(SEARCH_BLOGNAME)) {
+				if(added==0)
+				{
+					builder.append("blogname");
+					added = 1;
+				}
+				else
+				{
+					builder.append("|blogname");
+				}
+				
+			}
+			
+			if(getParameterAsBoolean(SEARCH_BLOGURL)) {
+				if(added==0)
+				{
+					builder.append("blogurl");
+					added = 1;
+				}
+				else
+				{
+					builder.append("|blogurl");
+				}	
+			}
+
+			if(getParameterAsBoolean(SEARCH_LINKS)) {
+				if(added==0)
+				{
+					builder.append("links");
+					added = 1;
+				}
+				else
+				{
+					builder.append("|links");
+				}	
+			}
+			
+			if(getParameterAsBoolean(SEARCH_TAGS)) {
+				if(added==0)
+				{
+					builder.append("tags");
+					added = 1;
+				}
+				else
+				{
+					builder.append("|tags");
+				}	
+			}
+			
+			if(getParameterAsBoolean(SEARCH_TEXT)) {
+				if(added==0)
+				{
+					builder.append("text");
+					added = 1;
+				}
+				else
+				{
+					builder.append("|text");
+				}	
+			}
+			
+			if(getParameterAsBoolean(SEARCH_TITLE)) {
+				if(added==0)
+				{
+					builder.append("title");
+					added = 1;
+				}
+				else
+				{
+					builder.append("|title");
+				}	
+			}
+			if(getParameterAsBoolean(SEARCH_URL)) {
+				if(added==0)
+				{
+					builder.append("url");
+					added = 1;
+				}
+				else
+				{
+					builder.append("|url");
+				}	
+			}
+			
+		
+		
+	}
+		builder.append(" " + getParameterAsString(Query_TERM));
+		return builder.toString();
+	}
 }
+
+
